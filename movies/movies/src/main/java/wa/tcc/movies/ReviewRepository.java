9ecdeba3ceb5e3356d75d1870d67eb6db3357968
juliamaytm <juliamaytm@gmail.com>
2023-04-28 01:01:24 -0300

    Backend com Java

---
+++ movies/movies/src/main/java/wa/tcc/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package wa.tcc.movies;
+
+import com.mongodb.client.MongoDatabase;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ movies/movies/src/test/java/wa/tcc/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package wa.tcc.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
